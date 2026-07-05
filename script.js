7344a880af95c1f19ec3ef2acb48e6dbf8b1cf16
taha ijaz <tahaijaz670@gmail.com>
2026-07-05 11:37:55 -0700

    new

---
+++ script.js
@@ -0,0 +1,79 @@
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+function page4Animation(){
+    let elemC = document.querySelector("#elem-container")
+let fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter", ()=>{
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", ()=>{
+    fixed.style.display = "none"
+})
+
+let elems = document.querySelectorAll(".elem")
+elems.forEach( (e)=>{
+    e.addEventListener("mouseenter", ()=>{
+        let image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+        
+    })
+} )
+  
+
+}
+
+ function swiperAnimation(){
+      var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 100,
+    
+    });
+ }
+ function menuAnimation(){
+    let menu = document.querySelector("nav h3")
+let full = document.querySelector("#full-scr")
+let navimg = document.querySelector("nav img") 
+let flag = 0
+menu.addEventListener("click", ()=>{
+
+    if(flag == 0){
+          full.style.top = 0
+          navimg.style.opacity = 0
+          flag =1
+    }else{
+         full.style.top = "-100%"
+          navimg.style.opacity = 1
+          flag =0
+    }
+  
+
+})
+
+}
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    },4200)
+}
+ swiperAnimation()
+ page4Animation()
+ menuAnimation()
+ loaderAnimation()
+
+//  setTimeout(() => {
+//     scroll.update();
+// }, 500);
+
+
+window.addEventListener("load", () => {
+    scroll.update();
+});
